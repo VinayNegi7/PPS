6b75428b6a3fb9ec5bc51bc79b3df6702c19366a
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-04-03 12:17:51 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,12 +1,25 @@
-/*Sugata mitra team
-program to input the sum of two no.
 #include <stdio.h>
-int a,b,c;
-void  main()
-{
-    a=10;
-    b=20;
-    c=a+b;
-    printf("%d",c);
-     
+#include <conio.h>
+void main(){
+int a;
+int b;
+printf(" enter the value of a and b");
+printf("\n a=");
+scanf("%d",&a);
+printf("\n b=");
+scanf("%d",&b);
+if(a>b)
+ {printf("a is the greatest and  value is %d",a);
+  }
+       else if (b>a)
+        {printf(" b is the greatest and the value is %d",b);
+         }
+          else
+           {printf(" both numbers are equal %d",a);
+           }
+
+          getch();
 }
+
+
+
