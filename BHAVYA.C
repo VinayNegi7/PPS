3586c89175fbb58356ca25b9ad70d94fb17a7995
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-04-04 11:41:07 +0530

    Add files via upload

---
+++ BHAVYA.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()//insurence for married males of age 25+
+{
+char G,martial;
+//gender is G
+int age;
+printf("Enter age,Gender,martial status:");
+scanf("%d,%c,%c" , &age,&G,&martial);
+if(martial=='M'&& age>25 && G=='M')
+{
+	printf("The Person is insured");
+}else
+{
+printf("The Person is not insured");
+}
+}
