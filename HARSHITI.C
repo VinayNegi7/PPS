e497b5295d667c808fa9103ead4b197665599841
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-04-04 11:12:05 +0530

    Add files via upload

---
+++ HARSHITI.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int G; float E,tax;
+clrscr();
+printf("enter the gender");
+scanf("%d",&G);
+// 0 means female, 1 means male
+if(G==1)
+{
+	printf("enter the income of employee");
+	scanf("%f",&E);
+	if(E>50000)
+		tax=(12.0/100.0)*(float)E;
+	else
+		tax=0.0;
+}
+else
+{
+	printf("enter the income of employee");
+	scanf("%f",&E);
+	if(E>70000)
+		tax=(10.0/100.0)*E;
+	else
+		tax=0.0;
+}
+
+printf("tax of employee= %f",tax);
+getch();
+}
+
+
