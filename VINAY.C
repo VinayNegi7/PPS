031dd35dfb02b9c9317a22e2a57f4d2d300e657e
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-05-09 11:14:31 +0530

    Add files via upload

--- VINAY.C
+++ VINAY.C
@@ -1,30 +1,29 @@
-/* Sugat mitra team
-program to calcualte loan installement*/
 #include<stdio.h>
 #include<conio.h>
 void main(){
-float loanamt,interest,monthlypayment;
-float i,firstmon,secondmon;
-  clrscr();
-printf("enter the loan amount");
-scanf("%f",&loanamt);
-printf("interest rate");
-scanf("%f",&interest);
-printf("montly payment");
-scanf("%f",&monthlypayment);
-//interest calculation//
-i=loanamt*((interest/100)/12);
-//amount with interest
-i=i+loanamt;
-firstmon=i-monthlypayment;
-//first month payment with interest//
-i=firstmon*((interest/100)/12);
-i=i+firstmon;
-secondmon=i-monthlypayment;
-//second month payment with interest//
-printf("remaning amount need to py after 1st installement:%2f\n",firstmon);
-printf("remaining amount need to pay after 2nd installement:%2f\n",secondmon);
+ int a,b,c;
+int ch;
+clrscr();
+printf("enter two numbers");
+scanf("%d%d",&a,&b);
+printf("enter choice");
+scanf("%d",&ch);
+
+switch(ch)
+{
+case 1: c=a+b;
+printf("%d",c);
+break;
+case 2: c=a-b;
+printf("%d",c);
+break;
+case 3: c=a*b;
+printf("%d",c);
+break;
+case 4: c=a/b;
+printf("%d",c);
+break;
+default: printf("invalid choice");
+}
 getch();
 }
-
-
