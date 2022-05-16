ec57766465827c23eacde3cb4fc991a3a0b0f674
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-05-16 10:39:49 +0530

    Add files via upload

---
+++ LINEAR.C
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int a[10],n,i,loc=0,item;
+clrscr();
+printf("how many elements");
+scanf("%d",&n);
+printf("enter array");
+for(i=0;i<n;i++);
+{
+scanf("%d",&a[i]);
+}
+printf("enter element to be searched");
+scanf("%d",&item);
+printf("linear search");
+for(i=0;i<n;i++)
+{if(a[i]==item)
+{loc=i+1;
+break;
+}
+}
+if(loc>0)
+printf("search successful");
+else
+printf("search unsucessful");
+getch();
+
+}
