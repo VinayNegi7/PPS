111ae5c06aabcb81c981193be58b0fa3a1b887d9
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-06-16 23:41:43 +0530

    Add files via upload

---
+++ reverse of array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int a[5] = {1,2,3,4,5};
+    int size = sizeof(a)/sizeof(a[0]);
+
+    for (int i = size-1;i>=0;i--){
+        printf("%d \n",a[i]);
+    }
+    return 0;
+}
