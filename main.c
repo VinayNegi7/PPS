1a92ba22677668321aa7a6c898b432dc1aabed35
Sugata Mitra <102474202+sugatamitra@users.noreply.github.com>
2022-06-18 13:19:33 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,27 +1,49 @@
-/*Sugat mitra team
-program to find biggest of two numbers*/
-#include <stdio.h>
-#include <conio.h>
-void main(){
-int a;
-int b;
-printf(" enter the value of a and b");
-printf("\n a=");
-scanf("%d",&a);
-printf("\n b=");
-scanf("%d",&b);
-if(a>b)
- {printf("a is the greatest and  value is %d",a);
-  }
-       else if (b>a)
-        {printf(" b is the greatest and the value is %d",b);
-         }
-          else
-           {printf(" both numbers are equal %d",a);
-           }
+#include<stdio.h>
+int main(){
+    int a[10][10],b[10][10],c[10][10],n,i,j,k;
 
-          getch();
+    printf("Enter the value of N: ");
+    scanf("%d",&n);
+    printf("Enter the elements of Matrix-A: \n");
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("Enter the elements of Matrix-B: \n");
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            scanf("%d",&b[i][j]);
+        }
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            c[i][j]=0;
+            for(k=0;k<n;k++)
+            {
+                c[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+
+    printf("The product of the two matrices is: \n");
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<n;j++)
+        {
+            printf("%d\t",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
-
-
-
